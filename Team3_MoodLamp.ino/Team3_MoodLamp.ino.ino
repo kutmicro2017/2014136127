49a87eabf527adfd5f04277f55f3d9b2e48d5eb9
ChoiSeonMun <ch002717@gmail.com>
2017-11-26 22:46:01 +0900

    TeamProject

--- Team3_MoodLamp.ino/Team3_MoodLamp.ino.ino
+++ Team3_MoodLamp.ino/Team3_MoodLamp.ino.ino
@@ -10,6 +10,7 @@ enum eColor { RED_TO_BLUE, ORANGE_TO_PURPLE, YELLOW_TO_CYAN };
 #define COLOR_BUTTON 2
 #define BRIGHTNESS_BUTTON 3
 #define TEMP_SENSOR A5
+#define BATTERY A0
 
 #define LOWEST_TEMPERATURE 0
 #define NORMAL_TEMPERATURE 15
@@ -17,11 +18,12 @@ enum eColor { RED_TO_BLUE, ORANGE_TO_PURPLE, YELLOW_TO_CYAN };
 
 volatile int gBrightness = 2;
 volatile int gColor = eColor::RED_TO_BLUE;
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, NEO_PIXEL, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel gCirclePixel = Adafruit_NeoPixel(16, NEO_PIXEL, NEO_GRB + NEO_KHZ800);
 
 void EmitRB(int temperature);
 void EmitOP(int temperature);
 void EmitYC(int temperature);
+void TurnOnPixel(int red, int green, int blue);
 
 void ChangeColor();
 void ChangeBrightness();
@@ -34,8 +36,8 @@ void setup()
     clock_prescale_set(clock_div_1); 
   }
 #endif
-  strip.begin();
-  strip.show();
+  gCirclePixel.begin();
+  gCirclePixel.show();
 /*터치센서
   pinMode(COLOR_BUTTON, INPUT_PULLUP);
   pinMode(BRIGHTNESS_BUTTON, INPUT_PULLUP);
@@ -43,14 +45,11 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(BRIGHTNESS_BUTTON), ChangeBrightness, LOW); 
 */
 
-/*온도측정
   Serial.begin(9600);
-  */
 }
 
 void loop()
 {
-  /*온도측정
   static const int thermistor = 4275;
   static const int R0 = 100000; // 100kelvin
 
@@ -58,28 +57,21 @@ void loop()
   float R = 1023.f / a - 1.f;
   R *= R0;
   float temperature = 1.f / (log(R / R0) / thermistor + 1 / 298.15f) - 273.15f;
-  */
 
-/*
+  Serial.print("Temp : ");
+  Serial.println(temperature);
+  
   switch (gColor)
   {
   case eColor::RED_TO_BLUE:
-    EmitRB(0);
+    EmitRB(temperature * 10.f);
     break;
   case eColor::ORANGE_TO_PURPLE:
-    EmitOP(0);
+    EmitOP(temperature * 10.f);
     break;
   default:
-    EmitYG(0);
+    EmitYC(temperature * 10.f);
     break;
-  }
-  */
-
-  
-  for (int i = 300; i >= 0; --i)
-  {
-    EmitOP(i);
-    delay(100);
   }
 }
 
@@ -104,11 +96,8 @@ void EmitRB(int temperature)
     green = 255 - 1.7f * (normalTemperature - temperature);
     blue = 255;
   }
-  for (int i = 0; i < gBrightness; ++i)
-  {
-    strip.setPixelColor(i, red, green, blue);
-  }
-  strip.show();
+
+  TurnOnPixel(red, green, blue);
 }
 void EmitOP(int temperature)
 {
@@ -131,11 +120,8 @@ void EmitOP(int temperature)
     green = 255 - 1.7f * (normalTemperature - temperature);
     blue = 255 - 0.23f * (normalTemperature - temperature);
   }
-  for (int i = 0; i < gBrightness; ++i)
-  {
-    strip.setPixelColor(i, red, green, blue);
-  }
-  strip.show();
+
+  TurnOnPixel(red, green, blue);
 }
 
 void EmitYC(int temperature)
@@ -159,12 +145,20 @@ void EmitYC(int temperature)
     green = 255;
     blue = 255;
   }
+
+  TurnOnPixel(red, green, blue);
+}
+
+void TurnOnPixel(int red, int green, int blue)
+{ 
+  gCirclePixel.clear();
   for (int i = 0; i < gBrightness; ++i)
   {
-    strip.setPixelColor(i, red, green, blue);
+    gCirclePixel.setPixelColor(i, red, green, blue);
   }
-  strip.show();
+  gCirclePixel.show();
 }
+
 void ChangeColor()
 {
   gColor = (gColor + 1) % 3;
